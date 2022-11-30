888ff12c6a3a7636e0831f94b6079aedb53a8e11
Yaya-SAKHO <yahyasakho97a@gmail.com>
2022-11-30 11:46:16 +0100

    correction

--- src/main/java/com/ipi/jva350/model/Entreprise.java
+++ src/main/java/com/ipi/jva350/model/Entreprise.java
@@ -71,10 +71,11 @@ public final class Entreprise {
 
     public static boolean bissextile(int y) {
         String tmp = String.valueOf(y);
-        if (tmp.charAt(2) == '1' || tmp.charAt(2) == '3' || tmp.charAt(2) == '5' || tmp.charAt(2) == '7' || tmp.charAt(2) == '9') {
-            if (tmp.charAt(3)=='2'||tmp.charAt(3)=='6') return true;
-            else
-                return false;
+        if (tmp.charAt(2) == '1' || tmp.charAt(2) == '3' || tmp.charAt(2) == '5' || tmp.charAt(2) == '7' || tmp.charAt(2) == '9')
+        {
+            if (tmp.charAt(3)=='2'||tmp.charAt(3)=='6'){
+                return true;
+            }
         }else{
             if (tmp.charAt(2) == '0' && tmp.charAt(3) == '0') {
                 return false;
